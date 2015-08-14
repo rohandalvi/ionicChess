c480709c5b00dde6d7c7f5aac6ae4a01191e9240
Rohan Dalvi <me@rohandalvi.com>
2015-08-13 22:52:00 -0400

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,108 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+        var board,
+            game = new Chess();
+
+// do not pick up pieces if the game is over
+// only pick up pieces for White
+        var onDragStart = function(source, piece, position, orientation) {
+            if (game.in_checkmate() === true || game.in_draw() === true ||
+                piece.search(/^b/) !== -1) {
+                return false;
+            }
+        };
+
+        var makeRandomMove = function() {
+            var possibleMoves = game.moves();
+
+            // game over
+            if (possibleMoves.length === 0) return;
+
+            var randomIndex = Math.floor(Math.random() * possibleMoves.length);
+            game.move(possibleMoves[randomIndex]);
+            board.position(game.fen());
+        };
+
+        var onDrop = function(source, target) {
+            // see if the move is legal
+            var move = game.move({
+                from: source,
+                to: target,
+                promotion: 'q' // NOTE: always promote to a queen for example simplicity
+            });
+
+            // illegal move
+            if (move === null) return 'snapback';
+
+            // make random legal move for black
+            window.setTimeout(makeRandomMove, 250);
+        };
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+        var onSnapEnd = function() {
+            board.position(game.fen());
+        };
+
+        var cfg = {
+            draggable: true,
+            position: 'start',
+            onDragStart: onDragStart,
+            onDrop: onDrop,
+            onSnapEnd: onSnapEnd
+        };
+        board = ChessBoard('board', cfg);
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
